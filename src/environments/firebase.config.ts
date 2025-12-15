1d669517d8168e41abb50826086c4a4c32179bec
Ahmad Sanoh <sanohahmad1996@gmail.com>
2025-12-15 18:27:28 +0100

    sanoh

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "YOUR API",
+  authDomain: "my-shop-app-a30cf.firebaseapp.com",
+  projectId: "my-shop-app-a30cf",
+  storageBucket: "my-shop-app-a30cf.firebasestorage.app",
+  messagingSenderId: "951126018250",
+  appId: "1:951126018250:web:f7b67671dcb7e4db7bcc33",
+  measurementId: "G-000N745HGP"
+};
